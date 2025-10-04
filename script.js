dfe043995137fe52362be36da796db16dd858412
latifaDevelopper <164013640+latifaDevelopper@users.noreply.github.com>
2025-10-04 12:38:21 +0100

    Update script.js
    Update script.js

--- script.js
+++ script.js
@@ -3,34 +3,34 @@ const alertBox = document.getElementById("customAlert");
 const closeAlert = document.getElementById("closeAlert");
 
 form.addEventListener("submit", e => {
-    e.preventDefault();
-    let isValid = true;
+  e.preventDefault();
+  let isValid = true;
 
-    const email = document.getElementById("email");
-    const password = document.getElementById("password");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
 
-    document.getElementById("emailError").textContent = "";
-    document.getElementById("passwordError").textContent = "";
+  document.getElementById("emailError").textContent = "";
+  document.getElementById("passwordError").textContent = "";
 
-    if(email.value.trim() === ""){
-        document.getElementById("emailError").textContent = "Email is required";
-        isValid = false;
-    } else if(!/\S+@\S+\.\S+/.test(email.value)){
-        document.getElementById("emailError").textContent = "Email is invalid";
-        isValid = false;
-    }
+  if(email.value.trim() === ""){
+    document.getElementById("emailError").textContent = "Email is required";
+    isValid = false;
+  } else if(!/\S+@\S+\.\S+/.test(email.value)){
+    document.getElementById("emailError").textContent = "Email is invalid";
+    isValid = false;
+  }
 
-    if(password.value.trim() === ""){
-        document.getElementById("passwordError").textContent = "Password is required";
-        isValid = false;
-    }
+  if(password.value.trim() === ""){
+    document.getElementById("passwordError").textContent = "Password is required";
+    isValid = false;
+  }
 
-    if(isValid){
-        alertBox.style.display = "flex";
-        form.reset();
-    }
+  if(isValid){
+    alertBox.style.display = "flex";
+    form.reset();
+  }
 });
 
 closeAlert.addEventListener("click", () => {
-    alertBox.style.display = "none";
+  alertBox.style.display = "none";
 });
